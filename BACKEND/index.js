00c2dc2ee9247c5fb25db6775ad363bed4f8a8d2
ashishthesatan <ashishthesatan@gmail.com>
2019-04-14 17:44:45 +0545

    initial prototype of server and main page of frontend

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -7,7 +7,7 @@ const debug = require('debug');
 const session = require('express-session');
 const handleBars = require('express-handlebars');
 const morgan = require('morgan');
-
+const fs = require('fs');
 app.engine('handlebars',handleBars({defaultLayout:'main'}));
 app.set('view engine','handlebars');
 app.use(express.static('public'));
@@ -26,6 +26,36 @@ app.get('/',(req,res)=>{
     console.log('request recieved!!');
     res.render('home');
     
+});
+app.get('/assets/video',(req,res)=>{
+  const path = 'assets/video.mkv';
+  const stat = fs.statSync(path);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-");
+    const start = parseInt(parts[0], 10);
+    const end = parts[1]
+      ? parseInt(parts[1], 10)
+      : fileSize-1;
+    const chunksize = (end-start)+1;
+    const file = fs.createReadStream(path, {start, end});
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    };
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(path).pipe(res);
+  }
 });
 //404 handler
 app.use((req,res,next)=>{
