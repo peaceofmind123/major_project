72bd608eea180d23b17ea74739016cdb7eb73ae1
ashishthesatan <ashishthesatan@gmail.com>
2019-04-18 14:12:15 +0545

    added post xhr code on the client side

--- BACKEND/public/js/data.js
+++ BACKEND/public/js/data.js
@@ -2,4 +2,21 @@ $(document).ready(function(){
     $("#ownerInput").select2({
         width:'resolve'
     });
-});
+    $('#vehicleDataSubmit').click((event)=>{
+        event.preventDefault();
+        $.ajax({
+            url:"/api/addUser",
+            dataType:'json',
+            type:'post',
+            contentType:'application/x-www-form-urlencoded',
+            success: function(data,textStatus,jqxhr){
+                $('#vehicleDataForm').trigger("reset");
+                },
+            error: function(jqxhr, textStatus,errorThrown) {
+                    console.log(textStatus,errorThrown);
+                }
+
+            });
+
+        });
+    });
